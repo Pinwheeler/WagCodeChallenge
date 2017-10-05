3880ee25ac7b6a470adf7d709780c24569c95174
Anthony Dreessen <anthonydreessen@gmail.com>
2017-10-05 10:23:14 -0700

    initial commit

---
+++ WagCodeChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WagCodeChallenge
+//
+//  Created by Anthony Dreessen on 10/5/17.
+//  Copyright © 2017 Anthony Dreessen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WagCodeChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WagCodeChallenge
+//
+//  Created by Anthony Dreessen on 10/5/17.
+//  Copyright © 2017 Anthony Dreessen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
